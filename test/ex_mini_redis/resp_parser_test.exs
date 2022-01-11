5c9a3dc06665188bc7d585aabae38cc52db46a33
kw7oe <choongkwern@hotmail.com>
2022-01-11 22:13:09 +0800

    Fix my stupid copy pasta test cases

--- test/ex_mini_redis/resp_parser_test.exs
+++ test/ex_mini_redis/resp_parser_test.exs
@@ -1,32 +1,12 @@
 defmodule ExMiniRedis.RESPParserTest do
   use ExUnit.Case, async: true
 
-  test "GET is encoded correctly" do
-    input = "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n"
+  test "encode and decode" do
+    reply = "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n"
+    assert {:ok, ["key", "GET"]} = ExMiniRedis.RESPParser.decode(reply)
 
-    correct_input =
-      ["GET", "key"]
-      # By default, Redix.Protocol.pack return iodata
-      # so it can be send with gen_tcp.send/2. So, we need
-      # to convert it to binary using IO.iodata_to_binary/1.
-      #
-      # If you're viewing this on Livebook, just hover over the
-      # `pack` word and you'll see the documentation.
-      |> Redix.Protocol.pack()
-      |> IO.iodata_to_binary()
-
-    assert input == correct_input
-  end
-
-  test "SET is encoded correctly" do
-    input = "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n"
-
-    correct_input =
-      ["SET", "key", "value"]
-      |> Redix.Protocol.pack()
-      |> IO.iodata_to_binary()
-
-    assert input == correct_input
+    list = ["GET", "key"]
+    assert "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n" = ExMiniRedis.RESPParser.encode(list)
   end
 
   test "Decoding partial input" do
@@ -40,9 +20,9 @@ defmodule ExMiniRedis.RESPParserTest do
     assert {:error, :incomplete_command} = ExMiniRedis.RESPParser.decode(input)
 
     input = "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n"
-    assert {:ok, ["GET", "key"]} = ExMiniRedis.RESPParser.decode(input)
+    assert {:ok, ["key", "GET"]} = ExMiniRedis.RESPParser.decode(input)
 
     input =  "*3\r\n$6\r\nCONFIG\r\n$3\r\nGET\r\n$4\r\nsave\r\n"
-    assert {:ok, ["CONFIG", "GET", "save"]} = ExMiniRedis.RESPParser.decode(input)
+    assert {:ok, ["save", "GET", "CONFIG"]} = ExMiniRedis.RESPParser.decode(input)
   end
 end
