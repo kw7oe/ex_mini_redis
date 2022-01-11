46490865a7b6cb07bd05a3a138c3cde83c932cdb
kw7oe <choongkwern@hotmail.com>
2022-01-11 22:28:05 +0800

    Remove redix from deps as we don't require it for test anymore

--- mix.exs
+++ mix.exs
@@ -24,7 +24,6 @@ defmodule ExMiniRedis.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:redix, "~> 1.0", only: :test}
     ]
   end
 end
